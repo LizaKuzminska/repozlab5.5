e3725608a33b7960fcaa70ecc55c2944c2ac3ce9
LizaKuzminska <yelyzavetakuzminska@gmail.com>
2023-11-21 17:24:46 +0200

    vssdvfc

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5_5.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double z;
+			int depth = 0;
+			z = St(5.0, 4, depth, 1);
+			Assert::AreEqual(z, 625, 0.00000001);
+
+		}
+	};
+}
---
+++ lab5_5.cpp
@@ -0,0 +1,35 @@
+﻿// lab5_5.cpp
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double St(double x, int n, int& depth, int level)
+{
+    if (level > depth)
+        depth = level;
+    cout << "level: " << level << endl;
+    // cout << "n = " << n << endl;
+    if (n == 0) {
+        return 1;
+    }
+    else if (n < 0) {
+        return 1.0 / St(x, abs(n), depth, level + 1);
+    }
+    else {
+        return x * St(x, n - 1, depth, level + 1);
+    }
+}
+
+int main() {
+    int depth = 0;
+    double x;
+    int n;
+    cout << "x = ";
+    cin >> x;
+    cout << "n = ";
+    cin >> n;
+    cout << "St = " << St(x, n, depth, 1) << endl;
+    cout << "Depth: " << depth << endl;
+
+    return 0;
+}
